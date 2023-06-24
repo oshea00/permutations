21f701a6db0321d0a8df1343ff395da03b15e371
Mike OShea <oshea00@gmail.com>
2023-06-24 09:34:04 -0700

    initial

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿var tokens = Permutations.GetPermutations(new List<char>{'A','B','C','D'}).ToList();
+var dict = new Dictionary<string,List<char>>();
+var stack = new Stack<TreeNode>();
+
+void BuildTree() {
+    while (stack.Count>0)
+    {
+        var node = stack.Pop();
+        if (node.Children.Count>0)
+        {
+            foreach (var c in node.Children) {
+                stack.Push(c);
+            }
+        }
+        else
+        {
+            var t = node.Value;        
+            for (int i=0; i < t.Count; i++) {
+                for (int j=i+1; j < t.Count; j++) {
+                    var tag = new List<char> { t[0],t[1],t[2],t[3] };
+                    (tag[i], tag[j]) = (tag[j], tag[i]);
+                    if (!dict.ContainsKey(String.Join("",tag)))
+                    {
+                        dict[String.Join("",tag)] = tag;
+                        var child = new TreeNode { Value = tag , Tag = String.Join("",tag)};
+                        node.Children.Add(child);
+                    }
+                }
+            }
+            stack.Push(node);
+        }
+    }
+}
+
+var node = new TreeNode { Value = tokens[0], Tag = String.Join("",tokens[0])};
+stack.Push(node);
+BuildTree();
+foreach (var s in stack)
+{
+    System.Console.WriteLine(s.Tag);
+}
+System.Console.WriteLine(node.Value);
+
+
+class TreeNode {
+    public IList<char> Value { get; set; } = null!;
+    public string Tag { get; set; } = null!;
+    public List<TreeNode> Children { get; set; } = new List<TreeNode>();
+}
+
+// foreach (var perm in perms)
+// {
+//     foreach (var p in perm)
+//     {
+//         Console.Write(p);
+//     }
+//     Console.WriteLine();
+// }
---
+++ README.md
@@ -0,0 +1,23 @@
+# Permutations
+Playing around with various algorithms. 
+
+## In-Place  
+DCBA
+01   02   03   12   13   23
+CDBA BCDA ACBD DBCA DABC DCAB
+
+BDCA
+01   02   03   12   13   23   
+DBCA CDBA ADCB BCDA BACD BDAC 
+
+DCAB
+01   02   03   12   13   23
+CDAB ACDB BCAD DACB DBAC DCBA 
+
+CABD
+01   02   03   12   13   23
+ACBD      DABC CBAD      CADB
+
+ABDC
+01   02   03   12   13   23
+BADC      CBDA ADBC      ABCD  
