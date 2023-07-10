5b677a81876e6984e5a6e794f2a044375da1942c
mikeo <oshea00@gmail.com>
2023-07-09 20:32:59 -0700

    add lines option

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 ﻿using System.Diagnostics;
 
 // Test list to permutate
-var list = new List<char>{'A','B','C','D','E'};
+var list = new List<char>{'A','B','C','D','E','F','G','H','I','J','K'};
 
 // Test implementations
-Profile(new PermutationsHeaps(), list, print: true);
+Profile(new PermutationsHeaps(), list, print: true, 100);
 //Profile(new PermutationsRecursive(),list, print: true);
 
 // Test Harness
-void Profile<T>(Permutations p, List<T> list, bool print = false) {
+void Profile<T>(Permutations p, List<T> list, bool print = false, int lines = 0) {
     var stopwatch = new Stopwatch();
     stopwatch.Start();
     var tokens = p.GetPermutations(list).ToList();
     stopwatch.Stop();
     if (print)
-        foreach(var t in tokens)
+        foreach(var t in tokens.Take(lines))
            System.Console.WriteLine(string.Join("",t));  
     System.Console.WriteLine($"Profiling {p.GetType().Name}:");
     System.Console.WriteLine($"Execution time {stopwatch.ElapsedMilliseconds} millisconds");
